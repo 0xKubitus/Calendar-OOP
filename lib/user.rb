11e9ee0bf79454490e67ca90b0ca5af221d2aa1b
0xKubitus <n.cubilier@gmail.com>
2022-04-25 17:17:00 +0200

    1step user OK

--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,17 @@
 require 'pry'
+
+#1) On doit créer une classe User:
 class User
+#2) Un User a 2 variables d'instance, un @email (string) et un @age (Integer),
+#que l'on peut lire et modifier à sa guise: 
+  attr_accessor :email, :age
+
+  def initialize(user_email, user_age)
+    @email = user_email
+    @age = user_age
+  end
+
 
 end
+
+binding.pry     
