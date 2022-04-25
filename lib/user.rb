fc400d0f354576299984e21bb883970f31e34ce1
0xKubitus <n.cubilier@gmail.com>
2022-04-25 17:34:03 +0200

    user.rb finito perfecto (je pense)

--- lib/user.rb
+++ lib/user.rb
@@ -4,14 +4,29 @@ require 'pry'
 class User
 #2) Un User a 2 variables d'instance, un @email (string) et un @age (Integer),
 #que l'on peut lire et modifier à sa guise: 
-  attr_accessor :email, :age
+  attr_accessor :age, :email
+  @@all_users = []
 
   def initialize(user_email, user_age)
     @email = user_email
     @age = user_age
+    @@all_users << self
   end
 
+  def self.all
+    return @@all_users
+  end
 
 end
 
-binding.pry     
+
+=begin 
+
+POUR TESTER FAIRE CE QUI SUIT:
+
+ kubi = User.new(27, "kubi@mail")
+ random = User.new(20, "random@rdm")
+
+ p User.all
+
+ =end
